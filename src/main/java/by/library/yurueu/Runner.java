5da63fda44fc473d412ec7aaaafec3e33b4329b2
Dobrowydka <andrikchannelyou@gmail.com>
2022-02-18 23:29:48 +0300

    runner

--- src/main/java/by/library/yurueu/Runner.java
+++ src/main/java/by/library/yurueu/Runner.java
@@ -1,5 +1,7 @@
 package by.library.yurueu;
 
+import by.library.yurueu.entity.Author;
+import by.library.yurueu.entity.Genre;
 import by.library.yurueu.entity.User;
 import by.library.yurueu.repository.*;
 import by.library.yurueu.repository.impl.*;
@@ -15,24 +17,15 @@ import static by.library.yurueu.service.Property.*;
 
 public class Runner {
     public static void main(String[] args) throws Exception {
-//        DataSource dataSource = JdbcConnectionPool.create(H2_URL, H2_USER, H2_PASSWORD);
-//        FlywayService flywayService = new FlywayService();
-//        flywayService.migrate();
+        DataSource dataSource = JdbcConnectionPool.create(H2_URL, H2_USER, H2_PASSWORD);
+        FlywayService flywayService = new FlywayService(H2_URL, H2_USER, H2_PASSWORD, MIGRATION_LOCATION);
+        flywayService.migrate();
 
 
-
-
-
-
-
-
-
-
-        User user = new User();
 //        BookRepository bookRepository = new BookRepositoryImpl(dataSource);
-//        GenreRepository genreRepository = new GenreRepositoryImpl(dataSource);
+        GenreRepositoryImpl genreRepository = new GenreRepositoryImpl(dataSource);
 //        UserRepository userRepository = new UserRepositoryImpl(dataSource);
-//        AuthorRepository authorRepository = new AuthorRepositoryImpl(dataSource);
+//        AuthorRepositoryImpl authorRepository = new AuthorRepositoryImpl(dataSource);
 //        OrderRepository orderRepository = new OrderRepositoryImpl(dataSource);
 //        BookCopyRepository bookCopyRepository = new BookCopyRepositoryImpl(dataSource);
 //        BookDamageRepository bookDamageRepository = new BookDamageRepositoryImpl(dataSource);
@@ -42,6 +35,8 @@ public class Runner {
 //        bookRepository.delete(2L);
 //        System.out.println(bookDamageRepository.findAll());
 //        System.out.println(bookCopyRepository.findAll());
-
+        System.out.println(genreRepository.findAll());
+        genreRepository.add(Genre.builder().genreName("tale").build());
+        System.out.println(genreRepository.findAll());
     }
 }
